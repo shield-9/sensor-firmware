8a44b2b039420690b2a4c1ed670b522e3d18d587
Daisuke Takahashi <daisuke@extendwings.com>
2017-11-06 15:42:56 +0900

    Update

---
+++ globals.h
@@ -0,0 +1,14 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+int1 is_observing = FALSE;
+unsigned int total_obs = 0;
+unsigned long long unixtime = 0;
+float latitude = 0.0, longitude = 0.0;
+
+extern is_observing;
+extern total_obs;
+extern unixtime;
+extern float latitude, longitude;
+
+#endif
